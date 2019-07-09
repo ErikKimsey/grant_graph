1796fd48decfcb1f8b80b1549ffda02bf89d1f6c
Erik Kimsey <kimseyerik@gmail.com>
2019-07-09 17:57:02 -0400

    Completed TreeComponent.js with bugs

--- src/components/Tree/TreeComponent.js
+++ src/components/Tree/TreeComponent.js
@@ -2,6 +2,25 @@ import React, { Component } from 'react';
 import * as d3 from 'd3';
 import Tree from './TreeD3';
 
+const data = {
+	name: 'Eric D',
+	children: [
+		{
+			name: 'Erik L',
+			children: [
+				{
+					name: 'Amon',
+					children: [ { name: 'Rabert' } ]
+				}
+			]
+		},
+		{
+			name: 'Isaac J',
+			children: [ { name: 'Scrooby' } ]
+		}
+	]
+};
+
 const styles = {
 	container: {
 		width: '833px',
@@ -14,18 +33,21 @@ class TreeComponent extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			data: []
+			data: null
 		};
 		this.containerRef = React.createRef();
 	}
 
 	componentDidMount() {
 		console.log('mointain');
+		this.setState({ data: data });
 	}
 
 	render() {
+		console.log(this.state.data);
+
 		return (
-			<div className="tree-component-container" style={styles.container} ref={this.containerRef}>
+			<div className="tree" style={styles.container} ref={this.containerRef}>
 				<Tree items={this.state.data} />
 			</div>
 		);
--- src/components/Tree/TreeD3.js
+++ src/components/Tree/TreeD3.js
@@ -3,24 +3,24 @@
 import React, { useEffect } from 'react';
 import * as d3 from 'd3';
 
-const data = {
-	name: 'Eric D',
-	children: [
-		{
-			name: 'Erik L',
-			children: [
-				{
-					name: 'Amon',
-					children: [ { name: 'Rabert' } ]
-				}
-			]
-		},
-		{
-			name: 'Isaac J',
-			children: [ { name: 'Scrooby' } ]
-		}
-	]
-};
+// const data = {
+// 	name: 'Eric D',
+// 	children: [
+// 		{
+// 			name: 'Erik L',
+// 			children: [
+// 				{
+// 					name: 'Amon',
+// 					children: [ { name: 'Rabert' } ]
+// 				}
+// 			]
+// 		},
+// 		{
+// 			name: 'Isaac J',
+// 			children: [ { name: 'Scrooby' } ]
+// 		}
+// 	]
+// };
 
 /**
  * Props:
@@ -29,17 +29,18 @@ const data = {
  *  - ??
  */
 const Tree = (props) => {
+	console.log(props);
 	useEffect(
 		() => {
 			d3.select('.tree > *').remove();
-			draw(props);
+			// draw(props);
 		},
-		[ props.items.length ]
+		[ props.data.length ]
 	);
 	return <div className="tree" />;
 };
 
-const draw = function() {
+const draw = function(props) {
 	/**
    * 
    * Needs defining:
@@ -104,14 +105,125 @@ const draw = function() {
 
 	function update(source) {
 		let data = tree(root);
-		let nodes = tree.descendents();
-	}
+		let nodes = tree.descendents(),
+			links = data.descendents.slice(1);
 
-	return {
-		width: width,
-		duration: duration,
-		svg: svg
-	};
+		nodes.forEach(function(d) {
+			d.y = d.depth * 180;
+		});
+
+		let node = svg.selectAll('g.node').data(nodes, function(d) {
+			return d.id || (d.id = ++i);
+		});
+
+		let nodeEnter = node
+			.enter()
+			.append('g')
+			.attr('class', 'node')
+			.attr('transform', function(d) {
+				return `translate(${source.y0}, ${source.x0})`;
+			})
+			.on('click', click);
+
+		nodeEnter
+			.append('text')
+			.attr('dy', '.35em')
+			.attr('x', function(d) {
+				return d.children || d.children ? -13 : 13;
+			})
+			.attr('text-anchor', function(d) {
+				return d.children || d._children ? 'end' : 'start';
+			})
+			.text(function(d) {
+				return d.data.name;
+			});
+
+		let nodeUpdate = nodeEnter.merge(node);
+
+		nodeUpdate.transition().duration(duration).attr('transform', function(d) {
+			return `translate(${d.y}, ${d.x})`;
+		});
+
+		nodeUpdate
+			.select('circle.node')
+			.attr('r', 20)
+			.style('fill', function(d) {
+				return d._children ? '#ef0772' : '#73ff6d';
+			})
+			.attr('cursor', 'pointer');
+
+		/**
+     * 
+     */
+		let nodeExit = node
+			.exit()
+			.transition()
+			.duration(duration)
+			.attr('transform', function(d) {
+				return `translate(${source.y},${source.x})`;
+			})
+			.remove();
+
+		nodeExit.select('circle').attr('r', 1e-6);
+
+		nodeExit.select('text').style('fill-opacity', 1e-6);
+
+		/**
+     * 
+     */
+		let link = svg.selectAll('path.link').data(links, function(d) {
+			return d.id;
+		});
+
+		let linkEnter = link.enter().insert('path', 'g').attr('class', 'link').attr('d', function(d) {
+			let o = { x: source.x0, y: source.y0 };
+			return diagonal(o, o);
+		});
+
+		/**
+     * 
+     */
+		let linkUpdate = linkEnter.merge(link);
+
+		linkUpdate.transition().duration(duration).attr('d', function(d) {
+			return diagonal(d, d.parent);
+		});
+
+		let linkExit = link
+			.exit()
+			.transition()
+			.duration(duration)
+			.attr('d', function(d) {
+				let o = { x: source.x, y: source.y };
+				return diagonal(o, o);
+			})
+			.remove();
+
+		nodes.forEach(function(d) {
+			d.x0 = d.x;
+			d.y0 = d.y;
+		});
+
+		function diagonal(s, d) {
+			let path = `M ${s.y} ${s.x}
+        C ${(s.y + d.y) / 2} ${s.x},
+        C ${(s.y + d.y) / 2} ${d.x},
+        ${d.y} ${d.x}
+      `;
+			return path;
+		}
+
+		function click(d) {
+			if (d.children) {
+				d._children = d.children;
+				d.children = null;
+			} else {
+				d.children = d._children;
+				d._children = null;
+			}
+			update(d);
+		}
+	}
 };
 
 export default Tree;
