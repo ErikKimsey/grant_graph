499a6fc2d96819023fa8cd58258d52a3830d6891
Erik Kimsey <kimseyerik@gmail.com>
2019-09-06 20:04:40 -0400

    Searching grant ID returns all relevant data from data-source in grantTypeSearch.js

--- src/utilities/grantTypeSearch.js
+++ src/utilities/grantTypeSearch.js
@@ -1,6 +1,16 @@
+const PARENT_KEYS = [ 'IC', 'source', 'code', 'received', 'awarded', 'received', 'rate', 'total' ];
 const grantTypeSearch = (input, data) => {
-	console.log(data);
-	console.log(input);
+	console.log(data['code']);
+	let obj = data['code'];
+	let codeNum = Object.keys(obj).find((key) => {
+		return obj[key] === input;
+	});
+	PARENT_KEYS.forEach((e, i) => {
+		console.log(data[e][codeNum]);
+		console.log(Object.values(data[e][codeNum]));
+	});
+
+	// console.log(data['code'].keys(input));
 };
 
 export default grantTypeSearch;
